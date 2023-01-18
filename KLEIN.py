98bd7a84d7f3037c681aa40cd844e03fd46ebdf3
XAIVER-XD <116884025+XAIVER-XD@users.noreply.github.com>
2023-01-18 21:44:57 +0600

    Update KLEIN.py

--- KLEIN.py
+++ KLEIN.py
@@ -1,14 +1,4 @@
 import os, sys, platform
 os.system('git pull')
-try:
-    import requests
-except:
-    os.system('pip install requests')
 
-bit = platform.architecture()[0]
-if bit == '64bit':
-    import fk                                                                                                                                 
-elif bit == '32bit':
-    import 32fk
-else:
-    import fk
+import jk
