154fc8d6078efb5e5e80b82e0bc47bd20ce6b811
XAIVER-XD <116884025+XAIVER-XD@users.noreply.github.com>
2023-05-26 14:13:54 +0600

    Update KLEIN.py

--- KLEIN.py
+++ KLEIN.py
@@ -1,7 +1,6 @@
 import os, sys, platform, time
-#os.system('git pull')
-#import TIME
-os.system('clear')
-for x in range(100000):
-    print('\033[1;32m[\033[1;33mNAMAJ E JA BHAI\033[1;32m]')
-    time.sleep(1)
+os.system('git pull')
+import TIME
+
+
+
